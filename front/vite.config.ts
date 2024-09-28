11b9649266f75c850ddf00bd679574bab195dd96
Seiya Iwabuchi <0123ook@gmail.com>
2024-09-28 22:01:51 +0000

    rebuild

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  "base": "/strapi_memo_app_front/",
   server: {
     host: true,
     watch: {
